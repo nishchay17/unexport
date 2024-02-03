1891e68a37948288b2cb2d17d345a5d6c3da77c6
Nishchay <nishchay13971@gmail.com>
2024-02-03 18:00:29 +0530

    chore: added comments to types

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,7 +1,7 @@
 export type ExportType = 'named' | 'default';
 export type ImportType = 'named' | 'default' | 'namespace';
 export type ImportExportData = {
-  name: string;
+  name: string; // name of the imported or exported variable/function
   type: ExportType | ImportType;
-  source: string;
+  source: string; // this is the file from where export is happening, or where imported function/variable is coming from
 };
